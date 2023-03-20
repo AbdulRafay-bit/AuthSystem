63c050da8cc4e0248f8861c1376894b92a1b2f30
AbdulRAfay-bit <inbox.abdulrafay@gmail.com>
2023-03-20 19:36:29 +0500

    firstcommit

---
+++ database/migrations/2023_03_20_131459_create_users.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) 
+        {
+            $table->id('id');// customer_id
+            $table->timestamps();  //created_at updated_at
+            $table->string('name',60);
+            $table->string('email',100);
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
